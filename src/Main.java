c74416b9dd17757f99779886aefff27147ba39ff
Таня Кишинская <lolo-20@yandex.ru>
2023-03-07 22:06:54 +0300

    add summ for second client

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int initialAccount = 100; //текущий счет
+        int amount = 1100; // входящие монеты
+        int isRegisred = 1000;   // при какой сумме начисояются балы
+
+
+        int percent;
+        if (amount >= isRegisred) {
+            percent = 1;
+        } else {
+            percent = 0;
+        }
+
+//         int percent = isRegisred ? 1 : 0;
+
+        int bonus = amount  / 100 * percent;
+        initialAccount = initialAccount + amount + bonus;
+        if (bonus > 500) {
+            bonus = 500;
+        }
+
+        System.out.println("Ваш баланс: " + initialAccount + "\nНачислено бонусов: " + bonus);
     }
+
+
 }
