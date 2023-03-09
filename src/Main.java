99ab4490d6e3207af8bf704abd628eb4795272b4
Таня Кишинская <lolo-20@yandex.ru>
2023-03-10 00:20:04 +0300

    изменение

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,18 @@ public class Main {
 
         int initialAccount = 100; //текущий счет
         int amount = 300; // входящие монеты
-        int isRegisred = 1000;   // при какой сумме начисояются балы
+        int sum = 1000;   // при какой сумме начисояются балы
 
 
         int percent;
-        if (amount >= isRegisred) {
+        if (amount >= sum) {
             percent = 1;
         } else {
             percent = 0;
         }
 
-//         int percent = isRegisred ? 1 : 0;
-
-        int bonus = amount  / 100 * percent;
+        int bonus = amount / 100 * percent;
         initialAccount = initialAccount + amount + bonus;
-        if (bonus > 500) {
-            bonus = 500;
-        }
 
         System.out.println("Ваш баланс: " + initialAccount + "\nНачислено бонусов: " + bonus);
     }
